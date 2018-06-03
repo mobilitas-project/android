529d3592bfe45840746a5e8654cd6273a4dac756
Victor Kazuyuki Shinya <contatovictorshinya@gmail.com>
2018-06-02 21:52:18 -0300

    Initial commit

---
+++ app/src/main/java/com/mobilitas/android/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.mobilitas.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
